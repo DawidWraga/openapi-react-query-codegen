809837d4437697ff92cf02ce5b2733375c3f8745
DawidWraga <dpwraga@gmail.com>
2023-07-08 17:03:01 +0100

    feat: add --useOptions to cli options

--- src/cli.ts
+++ src/cli.ts
@@ -1,37 +1,48 @@
 #!/usr/bin/env node
-import { generate } from "./generate";
-import { Command } from "commander";
-import packageJson from "../package.json";
-import { Options } from "openapi-typescript-codegen";
+import { generate } from './generate';
+import { Command } from 'commander';
+import packageJson from '../package.json';
+import { Options } from 'openapi-typescript-codegen';
 
 export type CLIOptions = {
-  output?: string;
-  client?: Options["httpClient"];
-} & Pick<Options, 'exportSchemas' | 'postfixModels' | 'postfixServices' | 'request' | 'indent' | 'input' | 'useUnionTypes'>;
+	output?: string;
+	client?: Options['httpClient'];
+} & Pick<
+	Options,
+	| 'exportSchemas'
+	| 'postfixModels'
+	| 'postfixServices'
+	| 'request'
+	| 'indent'
+	| 'input'
+	| 'useUnionTypes'
+	| 'useOptions'
+>;
 
 const program = new Command();
 
 program
-  .name("openapi-rq")
-  .version(packageJson.version)
-  .description("Generate React Query code based on OpenAPI")
-  .requiredOption(
-    "-i, --input <value>",
-    "OpenAPI specification, can be a path, url or string content (required)"
-  )
-  .option("-o, --output <value>", "Output directory", "openapi")
-  .option(
-    "-c, --client <value>",
-    "HTTP client to generate [fetch, xhr, node, axios, angular]",
-    "fetch"
-  )
-  .option("--useUnionTypes", "Use union types", false)
-  .option("--exportSchemas <value>", "Write schemas to disk", false)
-  .option("--indent <value>", "Indentation options [4, 2, tabs]", "4")
-  .option("--postfixServices <value>", "Service name postfix", "Service")
-  .option("--postfixModels <value>", "Modal name postfix")
-  .option("--request <value>", "Path to custom request file")
-  .parse();
+	.name('openapi-rq')
+	.version(packageJson.version)
+	.description('Generate React Query code based on OpenAPI')
+	.requiredOption(
+		'-i, --input <value>',
+		'OpenAPI specification, can be a path, url or string content (required)'
+	)
+	.option('-o, --output <value>', 'Output directory', 'openapi')
+	.option(
+		'-c, --client <value>',
+		'HTTP client to generate [fetch, xhr, node, axios, angular]',
+		'fetch'
+	)
+	.option('--useUnionTypes', 'Use union types', false)
+	.option('--exportSchemas <value>', 'Write schemas to disk', false)
+	.option('--indent <value>', 'Indentation options [4, 2, tabs]', '4')
+	.option('--postfixServices <value>', 'Service name postfix', 'Service')
+	.option('--postfixModels <value>', 'Modal name postfix')
+	.option('--request <value>', 'Path to custom request file')
+	.option('--useOptions', 'Use options or arguments functions', false)
+	.parse();
 
 const options = program.opts<CLIOptions>();
 
